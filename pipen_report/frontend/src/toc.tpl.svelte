6af4cecc960c32f68ab530a9cc6106201f0d7dc7
pwwang <pwwang@pwwang.com>
2021-10-27 15:37:32 -0700

    🩹 Expand the TOC by default

--- pipen_report/frontend/src/toc.tpl.svelte
+++ pipen_report/frontend/src/toc.tpl.svelte
@@ -20,7 +20,7 @@
         {#if h1_toc.children.length == 0}
         <SideNavLink href={`#${h1_toc.slug}`} title={h1_toc.text} text={h1_toc.text} />
         {:else}
-        <SideNavMenu href={`#${h1_toc.slug}`} title={h1_toc.text} text={h1_toc.text}>
+        <SideNavMenu href={`#${h1_toc.slug}`} title={h1_toc.text} text={h1_toc.text} expanded>
         {#each h1_toc.children as h2_toc}
             <SideNavMenuItem href={`#${h2_toc.slug}`} title={h2_toc.text} text={h2_toc.text} />
         {/each}
