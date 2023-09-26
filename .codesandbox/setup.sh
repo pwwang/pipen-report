83585aa4ee91cc5465a6326e124db314cec2caf6
justold <1188067+pwwang@users.noreply.github.com>
2023-09-26 03:03:20 +0000

    ⬆️ Update dependencies

--- .codesandbox/setup.sh
+++ .codesandbox/setup.sh
@@ -10,4 +10,5 @@ cd $WORKSPACE
 
 # Install whichpy
 WHICHPY="https://gist.githubusercontent.com/pwwang/879966128b0408c2459eb0a0b413fa69/raw/2f2573d191edec1937a2bf0873aa33a646b5ef29/whichpy.fish"
-curl -sS $WHICHPY -o ~/.config/fish/functions/whichpy.fish
+mkdir -p ~/.config/fish/functions/
+wget $WHICHPY -O ~/.config/fish/functions/whichpy.fish
