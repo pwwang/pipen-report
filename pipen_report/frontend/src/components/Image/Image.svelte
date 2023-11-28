079e8f0c3e6d65703e5e8fd419301b8be5d214d0
pwwang <pwwang@pwwang.com>
2023-11-28 17:20:48 -0600

    Add lazy loading for Image

--- pipen_report/frontend/src/components/Image/Image.svelte
+++ pipen_report/frontend/src/components/Image/Image.svelte
@@ -2,12 +2,48 @@
     import ImageInner from "./ImageInner.svelte";
 
     export let frameProps = { class: "pipen-report-image-frame" };
+    export let width = 200;
+    export let height = 100;
 </script>
 
 {#if !!frameProps}
     <div {...frameProps}>
-        <ImageInner {...$$restProps} />
+        <ImageInner {...$$restProps}>
+            <svelte:fragment slot="loading">
+                <div
+                    class="pipen-report-image-loading"
+                    style="--preset-width: {width}; --preset-height: {height};"
+                >
+                    Image loading ...
+                </div>
+            </svelte:fragment>
+            <svelte:fragment slot="error">
+                <div
+                    class="pipen-report-image-loading error"
+                    style="--preset-width: {width}; --preset-height: {height};"
+                >
+                    Image loading error!
+                </div>
+            </svelte:fragment>
+        </ImageInner>
     </div>
 {:else}
-    <ImageInner {...$$restProps} />
+    <ImageInner {...$$restProps}>
+        <svelte:fragment slot="loading">
+            <div
+                class="pipen-report-image-loading"
+                style="--preset-width: {width}; --preset-height: {height};"
+            >
+                Image loading ...
+            </div>
+        </svelte:fragment>
+        <svelte:fragment slot="error">
+            <div
+                class="pipen-report-image-loading error"
+                style="--preset-width: {width}; --preset-height: {height};"
+            >
+                Image loading error!
+            </div>
+        </svelte:fragment>
+    </ImageInner>
 {/if}
