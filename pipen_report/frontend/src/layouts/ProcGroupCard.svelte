297cf3f461083f7a1be81c197b6b5054ae8db4e5
pwwang <pwwang@pwwang.com>
2023-12-02 18:38:34 -0600

    Make proc group card on index page expand on top, instead of bottom, when short of space

--- pipen_report/frontend/src/layouts/ProcGroupCard.svelte
+++ pipen_report/frontend/src/layouts/ProcGroupCard.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { onMount } from "svelte";
     import { Dropdown } from "carbon-components-svelte";
 
     export let name;
@@ -11,9 +12,25 @@
         {id: pgId, text: `${name} ...`, desc},
         ...procs.map(proc => ({id: proc.name, text: proc.name, desc: proc.desc}))
     ];
+
+    let direction = "bottom";
+    let ref;
+
+    onMount(() => {
+        const rect = ref.getBoundingClientRect();
+        const bottom_space = window.innerHeight - rect.bottom - rect.height - 10;
+        const top_space = rect.top - rect.height - 10;
+
+        if (bottom_space < 0 && top_space > 0) {
+            direction = "top";
+        }
+    });
+
 </script>
 
 <Dropdown
+    bind:ref
+    {direction}
     size="xl"
     hideLabel
     {items}
@@ -30,3 +47,10 @@
     <h2>{item.text}</h2>
     {item.desc || ""}
 </Dropdown>
+
+<style>
+    :global(.bx--list-box__menu) {
+        border-radius: 0.6rem;
+        border: 1px solid var(--cds-border-subtle-selected);
+    }
+</style>
