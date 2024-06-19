988e04b820f48cec0ffc47eb8023b6d3110257c7
pwwang <pwwang@pwwang.com>
2024-06-18 21:45:58 -0500

    deps: bump pipen to 0.14.6 and other deps to latest version

--- tests/pipelines/force_export.py
+++ tests/pipelines/force_export.py
@@ -39,7 +39,7 @@ def pipeline(**config):
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         pipeline(
-            plugins=["no:args"],
+            plugins=["-args"],
             workdir=sys.argv[1],
             outdir=sys.argv[2],
         ).run()
