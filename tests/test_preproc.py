fa44959da8cd1b81a53602ea963647aac29f1944
pwwang <pwwang@pwwang.com>
2024-08-16 21:09:41 -0700

    test: move all forked test in front

--- tests/test_preproc.py
+++ tests/test_preproc.py
@@ -14,6 +14,39 @@ from pipen_report.preprocess import (
 from . import run_pipeline
 
 
+@pytest.mark.forked
+def test_regular(tmp_path):
+    run_pipeline("regular", _dir=tmp_path)
+    report = tmp_path / "outdir" / "REPORTS" / "pages" / "_index.js"
+    assert report.exists()
+
+
+@pytest.mark.forked
+def test_markdown(tmp_path):
+    run_pipeline("markdown_", _dir=tmp_path)
+    report = tmp_path.joinpath(
+        "workdir",
+        "Markdown-process",
+        ".report-workdir",
+        "src",
+        "pages",
+        "Process2",
+        "proc.svelte",
+    )
+    assert "<h1>Process</h1>" in report.read_text()
+
+
+@pytest.mark.forked
+def test_extlibs(tmp_path):
+    run_pipeline("extlibs", _dir=tmp_path)
+    report = tmp_path / "outdir" / "REPORTS" / "pages" / "Index.js"
+    text = report.read_text()
+    assert "Hello world" in text
+    assert "A link" in text
+    assert "../Index/12.txt" in text
+
+
+
 @pytest.mark.parametrize(
     "source, index, page, kind, text, expected",
     [
@@ -218,35 +251,3 @@ def test_preprocess2(tmp_path):
         ],
     )
     assert preprocess(source, basedir, True, 1) == expected
-
-
-# @pytest.mark.forked
-def test_regular(tmp_path):
-    run_pipeline("regular", _dir=tmp_path)
-    report = tmp_path / "outdir" / "REPORTS" / "pages" / "_index.js"
-    assert report.exists()
-
-
-@pytest.mark.forked
-def test_markdown(tmp_path):
-    run_pipeline("markdown_", _dir=tmp_path)
-    report = tmp_path.joinpath(
-        "workdir",
-        "Markdown-process",
-        ".report-workdir",
-        "src",
-        "pages",
-        "Process2",
-        "proc.svelte",
-    )
-    assert "<h1>Process</h1>" in report.read_text()
-
-
-@pytest.mark.forked
-def test_extlibs(tmp_path):
-    run_pipeline("extlibs", _dir=tmp_path)
-    report = tmp_path / "outdir" / "REPORTS" / "pages" / "Index.js"
-    text = report.read_text()
-    assert "Hello world" in text
-    assert "A link" in text
-    assert "../Index/12.txt" in text
