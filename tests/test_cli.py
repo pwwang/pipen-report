bbf4c856b2b6c3dcb75e7da1fc80267fdfdb865c
pwwang <pwwang@pwwang.com>
2025-10-03 00:06:58 -0700

    tests: update subprocess calls to use sys.executable
    - Modified subprocess calls in test_update_first_time and test_config to use sys.executable for consistency.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,6 @@
 import pytest  # noqa
 
+import sys
 import subprocess as sp
 
 
@@ -18,13 +19,20 @@ def test_update_first_time(tmp_path):
     nmdir = tmp_path / "nmdir"
     nmdir.mkdir()
     out = sp.Popen(
-        ["pipen", "report", "config", "--local", "--nmdir", str(nmdir)],
+        [
+            sys.executable, "-m",
+            "pipen", "report", "config", "--local", "--nmdir", str(nmdir)
+        ],
         cwd=str(tmp_path),
         stdout=sp.PIPE,
     )
     assert out.wait() == 0
 
-    out = sp.Popen(["pipen", "report", "update"], cwd=str(tmp_path), stdout=sp.PIPE)
+    out = sp.Popen(
+        [sys.executable, "-m", "pipen", "report", "update"],
+        cwd=str(tmp_path),
+        stdout=sp.PIPE,
+    )
     assert out.wait() == 0
     stdout = out.stdout.read().decode()
     assert "The frontend directory:" in stdout
@@ -34,13 +42,16 @@ def test_update_first_time(tmp_path):
 @pytest.mark.forked
 def test_config(tmp_path):
 
-    out = sp.Popen(["pipen", "report", "config", "--list"], stdout=sp.PIPE)
+    out = sp.Popen(
+        [sys.executable, "-m", "pipen", "report", "config", "--list"],
+        stdout=sp.PIPE,
+    )
     assert out.wait() == 0
     stdout = out.stdout.read().decode()
     assert "Note that these values can still be overwritten by" in stdout
 
     out = sp.Popen(
-        ["pipen", "report", "config", "--local", "--npm", "npm2"],
+        [sys.executable, "-m", "pipen", "report", "config", "--local", "--npm", "npm2"],
         cwd=str(tmp_path),
     )
     assert out.wait() == 0
